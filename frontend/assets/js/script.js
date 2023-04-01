69c143000e30e5d7093e7c0c60d447d7d682a182
Djavan <djavancosta93@gmail.com>
2023-04-01 17:14:06 -0300

    Lógica de virar as cartas

--- frontend/assets/js/script.js
+++ frontend/assets/js/script.js
@@ -1,37 +1,92 @@
 "use strict";
 
-const numeros = [...Array(10).keys()].map((i) => i.toString());
+const numeros = [...Array(3).keys()].map((i) => i.toString());
+
+window.onload = () => {
+  sessionStorage.removeItem('idCarta')
+  criarCartas();
+}
 
 const sortearCartas = (cartas) => {
-    return embaralhar(duplicar(cartas));
+  return embaralhar(duplicar(cartas.map((carta, i) => ({ carta, id: i }))));
 }
 
 const duplicar = (lista) => {
-    return [...lista, ...lista];
+  return [...lista, ...lista];
 }
 
 const embaralhar = (lista) => {
-    return lista.map(a => [a, Math.random()])
-        .sort((a, b) => a[1] > b[1])
-        .map(a => a[0]);
+  return lista.map(a => [a, Math.random()])
+    .sort((a, b) => a[1] - b[1])
+    .map(a => a[0]);
 }
 
-const virarCard = () => {
-    const cartaJaVirada = true;
-    if (cartaJaVirada) return;
+const virarCarta = (carta) => {
+  const idCartaAtual = carta.getAttribute('data-index');
+  const cartaJaVirada = carta.getAttribute('data-active') === 'on';
+  if (cartaJaVirada) return;
 
-    const idCartaAtual = '' // Obter um id único para cada carta
+  carta.setAttribute('data-active', 'on');
 
-    const idPrimeiraCarta = sessionStorage.getItem('idCarta');
-    const naoVirouOutraCarta = idCarta == undefined;
-    if (naoVirouOutraCarta) {
-        sessionStorage.setItem('idCarta', idCartaAtual);
-        return;
+  const idPrimeiraCarta = sessionStorage.getItem('idCarta');
+  if (idPrimeiraCarta === null) {
+    sessionStorage.setItem('idCarta', idCartaAtual);
+    return;
+  }
+
+  const listaCartas = document.getElementById('cardLocations').children;
+
+  if (idPrimeiraCarta !== idCartaAtual) {
+    setTimeout(() => {
+      for (const elementoCarta of listaCartas) {
+        if (idPrimeiraCarta === elementoCarta.getAttribute('data-index')) {
+          elementoCarta.setAttribute('data-active', 'off');
+        }
+      }
+      carta.setAttribute('data-active', 'off');
+    }, 1000);
+    sessionStorage.removeItem('idCarta');
+    return;
+  }
+
+  // Aumentar pontuação
+  for (let card of listaCartas) {
+    if ([idCartaAtual, idPrimeiraCarta].includes(card.getAttribute('data-index'))) {
+      card.setAttribute('class', 'card done');
+      card.removeAttribute('data-active');
+      card.removeAttribute('onClick');
+      sessionStorage.removeItem('idCarta');
     }
+  }
 
-    if (idPrimeiraCarta == idCartaAtual) {
-        // Detalhes de pontuação
-    } else {
-        // Desvirar as duas cartas
+  for (let card of listaCartas) {
+    if (card.getAttribute('data-active') === "off") {
+      return;
     }
+  }
+
+  // Ganhou
+  document.getElementsByClassName('success-message-mask')[0].setAttribute('class', 'success-message-mask show');
+  document.getElementsByClassName('success-message')[0].setAttribute('class', 'success-message show');
+}
+
+const getNovaCarta = (idCarta, verso) => {
+  return `<div class="card" data-index="${idCarta}" data-active="off" onClick="virarCarta(this)">
+    <div class="card-inner">
+      <div class="card-front">
+        <p>? - ${idCarta}</p>
+      </div>
+      <div class="card-back">
+        <p class="card-icon">${verso}</p>
+      </div>
+    </div>
+  </div>`;
+}
+
+const criarCartas = () => {
+  const cartas = sortearCartas(numeros);
+  for (const carta of cartas) {
+    const novaCarta = getNovaCarta(carta['id'], carta['carta']);
+    document.getElementById('cardLocations').innerHTML += novaCarta;
+  }
 }
